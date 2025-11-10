2a7669dd1cc45b2b084c3072afe3a81c573e865f
Superredstone <patrickcanal3@gmail.com>
2025-11-10 09:10:14 +0100

    Removed bridge from workstation

--- machines/workstation/networking.nix
+++ machines/workstation/networking.nix
@@ -4,15 +4,15 @@
 	networking.firewall.allowedUDPPorts = [ 8000 23 ];
 	networking.firewall.allowedTCPPorts = [ 8000 23 ];
 	networking.useDHCP = false;
-	networking.bridges = {
-		"br0" = {
-			interfaces = [ "eno1" ];
-		};
-	};
-	networking.interfaces.br0.ipv4.addresses = [{
-		address = "192.168.1.235";
-		prefixLength = 24;
-	}];
+	# networking.bridges = {
+	# 	"br0" = {
+	# 		interfaces = [ "eno1" ];
+	# 	};
+	# };
+	# networking.interfaces.br0.ipv4.addresses = [{
+	# 	address = "192.168.1.235";
+	# 	prefixLength = 24;
+	# }];
 	networking.defaultGateway = "192.168.1.1";
 	networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 	hardware.bluetooth.enable = true;
