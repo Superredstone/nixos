2bd5f95001c6d751eb6ecaeb2e5c83ed9d15aa7b
Superredstone <patrickcanal3@gmail.com>
2026-01-09 18:38:13 +0100

    Disabled Gnome extensions in home manager because are not working

--- home/gnome.nix
+++ home/gnome.nix
@@ -2,14 +2,14 @@
 {
 	dconf = {
 		settings."org/gnome/shell" = {
-			disable-user-extensions = false;
-			enabled-extensions = with pkgs.gnomeExtensions; [
-				blur-my-shell.extensionUuid
-				dash-to-dock.extensionUuid
-				dash-to-panel.extensionUuid
-				caffeine.extensionUuid
-				timer.extensionUuid
-			];
+			# disable-user-extensions = false;
+			# enabled-extensions = with pkgs.gnomeExtensions; [
+			# 	blur-my-shell.extensionUuid
+			# 	dash-to-dock.extensionUuid
+			# 	dash-to-panel.extensionUuid
+			# 	caffeine.extensionUuid
+			# 	timer.extensionUuid
+			# ];
 		};
 	};
 }
