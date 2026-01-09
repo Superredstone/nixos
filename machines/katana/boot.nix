a2bd6084b75f81a3ddd86e7332294a62cf2bd3b0
Superredstone <patrickcanal3@gmail.com>
2026-01-09 22:15:36 +0100

    Added plymouth

--- machines/katana/boot.nix
+++ machines/katana/boot.nix
@@ -1,8 +1,24 @@
 { pkgs, ... }:
 {
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.loader.systemd-boot.configurationLimit = 10;
-  	boot.initrd.luks.devices."luks-563ebcd9-614b-460f-af9a-5cf1d9c8bef5".device = "/dev/disk/by-uuid/563ebcd9-614b-460f-af9a-5cf1d9c8bef5";
-	boot.kernelPackages = pkgs.linuxPackages_zen; # pkgs.linuxPackages_latest;
+	boot = {
+		loader.systemd-boot.enable = true;
+		loader.efi.canTouchEfiVariables = true;
+		loader.systemd-boot.configurationLimit = 10;
+  		initrd = {
+			luks.devices."luks-563ebcd9-614b-460f-af9a-5cf1d9c8bef5".device = "/dev/disk/by-uuid/563ebcd9-614b-460f-af9a-5cf1d9c8bef5";
+			verbose = false;
+		};
+		kernelPackages = pkgs.linuxPackages_zen; # pkgs.linuxPackages_latest;
+		consoleLogLevel = 3;
+		kernelParams = [
+			"quiet"
+			"splash"
+			"intremap=on"
+			"boot.shell_on_fail"
+			"udev.log_priority=3"
+			"rd.systemd.show_status=auto"
+		];	
+
+		plymouth.enable = true;
+	};
 }
