e034b24fe783d1fd0856760098d6e508ea2091f4
Superredstone <patrickcanal3@gmail.com>
2026-02-13 07:59:52 +0100

    feat(home): fix tmux colors and scrolling

--- home/tmux.nix
+++ home/tmux.nix
@@ -3,7 +3,9 @@
   programs.tmux = {
     enable = true;
     escapeTime = 0;
+    mouse = true;
     extraConfig = ''
+            set -g default-terminal "screen-256color"
       			set -g @catppuccin_flavor 'mocha'
       		'';
     plugins = with pkgs; [
