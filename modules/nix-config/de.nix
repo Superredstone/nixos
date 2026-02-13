381b0cec9150f646d654de9a325178068a8253c8
Superredstone <patrickcanal3@gmail.com>
2026-02-13 10:24:34 +0100

    feat(gnome): fix gstreamer missing plugins

--- modules/nix-config/de.nix
+++ modules/nix-config/de.nix
@@ -60,4 +60,13 @@
       else
         pkgs.kdePackages.kdeconnect-kde;
   };
+
+  # Hack to fix "Your GStreamer installation is missing a plug-in." inside of Nautilus
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+    lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0"
+      [
+        pkgs.gst_all_1.gst-plugins-good
+        pkgs.gst_all_1.gst-plugins-bad
+        pkgs.gst_all_1.gst-plugins-ugly
+      ];
 }
