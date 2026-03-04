8574e4d19aa49dedd311c5275b38a2181066e075
Superredstone <patrickcanal3@gmail.com>
2026-03-04 22:07:44 +0100

    feat(services): add host key generation

--- modules/nix-config/services.nix
+++ modules/nix-config/services.nix
@@ -2,5 +2,14 @@
 {
   services = {
     pcscd.enable = true;
+    openssh = {
+      enable = true;
+      hostKeys = [
+        {
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          type = "ed25519";
+        }
+      ];
+    };
   };
 }
