3c72f86734086bdcffe998bc5c13954fe7417606
Superredstone <patrickcanal3@gmail.com>
2026-03-06 12:49:34 +0100

    fix(virt): disable libvirtd

--- machines/katana/virtualisation.nix
+++ machines/katana/virtualisation.nix
@@ -3,7 +3,7 @@
   programs.virt-manager.enable = true;
   virtualisation = {
     docker.enable = true;
-    libvirtd.enable = true;
+    # libvirtd.enable = true; disable temorarly because of https://github.com/NixOS/nixpkgs/issues/496836
     spiceUSBRedirection.enable = true;
     waydroid.enable = true;
   };
