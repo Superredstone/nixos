aac20b0c009a3a1fa1299935847aeeb1d5e60844
Superredstone <patrickcanal3@gmail.com>
2026-05-25 09:02:43 +0200

    feat(bomba): remove docker firewall rules

--- machines/bomba/networking.nix
+++ machines/bomba/networking.nix
@@ -1,7 +1,4 @@
 { ... }:
-let
-  dockerNetwork = "172.18.0.0/16";
-in
 {
   networking = {
     networkmanager.enable = true;
@@ -22,10 +19,6 @@ in
         80
         443
       ];
-      extraInputRules = ''
-        ip saddr ${dockerNetwork} tcp dport 5900 accept
-        ip saddr ${dockerNetwork} tcp dport 8001-8005 accept
-      '';
     };
   };
 }
