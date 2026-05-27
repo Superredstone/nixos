eeae6d4647652619746d7251ccdbebc9af148f99
Superredstone <patrickcanal3@gmail.com>
2026-05-27 22:36:39 +0200

    feat(octoprint): get octoprint plugin from my-features branch

--- machines/bomba/services/octoprint.nix
+++ machines/bomba/services/octoprint.nix
@@ -1,32 +1,12 @@
-{ ... }:
+{ pkgs-my-features, ... }:
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      octoprint = super.octoprint.override {
-        packageOverrides = pyself: pysuper: {
-          octoprint-fanspeedslider = pyself.buildPythonPackage rec {
-            pname = "fanspeedslider";
-            version = "0.2.3";
-            src = self.fetchFromGitHub {
-              owner = "mival";
-              repo = "OctoPrint-FanSpeedSlider";
-              rev = "${version}";
-              sha256 = "sha256-0Gp9EgaKyCnRQv4A1sNHquTRqjag2Oz/fDGVVo95FfY=";
-            };
-            propagatedBuildInputs = [ pysuper.octoprint ];
-            pyproject = true;
-            doCheck = false;
-          };
-        };
-      };
-    })
-  ];
   services.octoprint = {
     enable = true;
     port = 8002;
+    package = pkgs-my-features.octoprint;
     plugins =
       plugins: with plugins; [
-        octoprint-fanspeedslider
+        fanspeedslider
       ];
   };
 }
