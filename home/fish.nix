817ed30cdb135efdf4351e7b4fffad27b02dd4bc
Superredstone <patrickcanal3@gmail.com>
2026-05-28 10:33:37 +0200

    feat(fish): add some aliases
    feat(fish): add some aliases

--- home/fish.nix
+++ home/fish.nix
@@ -2,6 +2,14 @@
 {
   programs.fish = {
     enable = true;
+    shellAliases = {
+      "please" = "sudo $(fc -ln -1)";
+      "amend" = "git commit --amend";
+      "commit" = "git commit";
+      "add" = "git add .";
+      "ssh" = "kitten ssh"; 
+      "dev" = "nix develop --command 'fish'";
+    };
     interactiveShellInit = ''
       			# This is commented until a good way of installing themes is found
       			# fish_config theme choose Catppuccin\ Mocha
@@ -12,16 +20,6 @@
       				  krabby random --no-title --padding-left 2
               end
       			end
-       
-      			function weather
-      				curl "wttr.in/Bolzano?0?Q?" 
-      			end
-
-      			function dev
-      				nix develop --command "fish"
-      			end
-
-            alias ssh "kitten ssh"
       		'';
     plugins = with pkgs.fishPlugins; [
       {
