f9688515569375a4f8790166a19bb927c3778f71
Superredstone <patrickcanal3@gmail.com>
2026-05-30 10:15:28 +0200

    feat(bomba): bump to 26.05

--- machines/bomba/default.nix
+++ machines/bomba/default.nix
@@ -14,5 +14,5 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "26.05"; # Did you read the comment?
 }
