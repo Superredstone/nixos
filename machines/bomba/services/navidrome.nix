941fa9f0b774c147c0bf105dfd8e3f91565c3ad1
Superredstone <patrickcanal3@gmail.com>
2026-06-28 19:09:33 +0200

    fix(bomba): remove navidrome plugins

--- machines/bomba/services/navidrome.nix
+++ machines/bomba/services/navidrome.nix
@@ -2,9 +2,6 @@
 {
   services.navidrome = {
     enable = true;
-    plugins = with pkgs.navidromePlugins; [
-      discord-rich-presence
-    ];
     settings = {
       Port = 8013;
       MusicFolder = "/var/lib/nixflix/media/music/";
