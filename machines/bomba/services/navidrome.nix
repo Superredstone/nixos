097a835e7f3ba7532b2f414548df421545e7181c
Superredstone <patrickcanal3@gmail.com>
2026-06-28 18:33:36 +0200

    feat(bomba): add navidrome plugin

--- machines/bomba/services/navidrome.nix
+++ machines/bomba/services/navidrome.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.navidrome = {
     enable = true;
+    plugins = with pkgs.navidromePlugins; [
+      discord-rich-presence
+    ];
     settings = {
       Port = 8013;
       MusicFolder = "/var/lib/nixflix/media/music/";
+      EnableSharing = true;
     };
   };
 }
