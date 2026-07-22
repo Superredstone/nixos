6496e7b6ed139f14870c46c5307cd03c8fdecae8
Superredstone <patrickcanal3@gmail.com>
2026-07-22 23:18:43 +0200

    feat(bomba): update nextcloud to 43

--- machines/bomba/services/nextcloud.nix
+++ machines/bomba/services/nextcloud.nix
@@ -8,7 +8,7 @@
   services = {
     nextcloud = {
       enable = true;
-      package = pkgs.nextcloud33;
+      package = pkgs.nextcloud34;
       hostName = "nextcloud.patrickcanal.it";
       https = false;
       config = {
