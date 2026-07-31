81006c28ba792911a631ad174890ad4adeb1f242
Superredstone <patrickcanal3@gmail.com>
2026-07-31 21:45:05 +0200

    feat(cache): remove garnix (broken)

--- modules/nix-config/caches.nix
+++ modules/nix-config/caches.nix
@@ -3,7 +3,6 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org"
-      "https://cache.garnix.io"
       "https://nixvim-superredstone.cachix.org"
       "https://noctalia.cachix.org"
       "https://attic.xuyh0120.win/lantian"
@@ -11,7 +10,6 @@
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nixvim-superredstone.cachix.org-1:mEXHVxEv5dKka3FOxTMFDfdk/DJ0baydsahi+zZIcQE="
       "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
